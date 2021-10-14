6cc6f58e37f0dd46064f6c57f1ebfe683f897232
mi nombre <vaseksindelaru@gmail.com>
2021-10-14 05:47:04 +0200

    app with axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios'
+
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 function App() {
+  useEffect (() => {axios.get()})
+  
   return (
     <div className="App">
      <h2>RGC</h2> 
