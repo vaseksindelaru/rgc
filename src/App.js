19fd1106d2f47b624866bdc30dd6fcf3782566b8
mi nombre <vaseksindelaru@gmail.com>
2021-10-14 13:07:41 +0200

    map de notas

--- src/App.js
+++ src/App.js
@@ -2,16 +2,36 @@ import './App.css';
 import React,{useState,useEffect} from 'react';
 import axios from 'axios'
 
-// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
 
 function App() {
   // useEffect (() => {axios.get()})
-  const [notes,setNotes] = useState ([])
+  /* const [notes,setNotes] = useState ([])
   const [newNotes,setNewNotes] = useState('')
   const [loading,setLoading] = useState (false)
 
 
- /*  useEffect(()=> {
+   useEffect(()=> {
     console.log('useEffect');
     setLoading(true);
 
@@ -24,13 +44,13 @@ function App() {
        setLoading(false);
       })
     },2000);
-  },[]) */
-
+  },[]) 
+ */
   
   return (
     <div className="App">
      <h2>RGC</h2> 
-     <p>{setNotes}</p>
+    <p>{notes.map(note=>(<strong>{note.id}</strong>))}</p> 
     </div>
   );
 }
