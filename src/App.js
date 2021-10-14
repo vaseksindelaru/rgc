26e7c2744e0d9ad907d31a0901f2410a4e576ff7
mi nombre <vaseksindelaru@gmail.com>
2021-10-14 06:03:41 +0200

    fetch

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import axios from 'axios'
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 function App() {
-  useEffect (() => {axios.get()})
+  // useEffect (() => {axios.get()})
+  fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+  .then(res => res.json())
+  .then(json => {console.log(json)})
   
   return (
     <div className="App">
