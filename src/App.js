678e6568b4db75dbff49626da9748dd347dfaee9
mi nombre <vaseksindelaru@gmail.com>
2021-10-14 12:17:16 +0200

    useEffect+loading

--- src/App.js
+++ src/App.js
@@ -6,13 +6,31 @@ import axios from 'axios'
 
 function App() {
   // useEffect (() => {axios.get()})
-  fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-  .then(res => res.json())
-  .then(json => {console.log(json)})
+  const [notes,setNotes] = useState ([])
+  const [newNotes,setNewNotes] = useState('')
+  const [loading,setLoading] = useState (false)
+
+
+ /*  useEffect(()=> {
+    console.log('useEffect');
+    setLoading(true);
+
+    setTimeout(()=>{
+    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => res.json())
+    .then(json => {
+       console.log('seteando las notas');
+       setNotes(json);
+       setLoading(false);
+      })
+    },2000);
+  },[]) */
+
   
   return (
     <div className="App">
      <h2>RGC</h2> 
+     <p>{setNotes}</p>
     </div>
   );
 }
