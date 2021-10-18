3d0ee377208af5c69a92633ee398f70c0a003ba8
mi nombre <vaseksindelaru@gmail.com>
2021-10-18 12:59:04 +0200

    axios

--- src/App.js
+++ src/App.js
@@ -15,9 +15,11 @@ function App()
   useEffect(()=>
   {
    console.log('useEffect')
-   fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-    .then (res=>res.json())
-    .then (json=>{console.log(json);setNotes(json)})
+   axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then ((res)=>{
+      const{data}=res
+      setNotes(data)
+      console.log(res)})
   },[])
  
   const handelChange = (evento) => 
